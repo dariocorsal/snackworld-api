4ae6382f407a1ece3447b3cc5928c38130f69859
dariocorsal <dariocscs@gmail.com>
2025-05-26 21:36:21 -0700

    Pruebas

--- src/routes/suscripcionRoutes.js
+++ src/routes/suscripcionRoutes.js
@@ -2,8 +2,12 @@ import express from "express";
 import {
   activarSuscripcion,
   obtenerSuscripcion,
+  obtenerTotalSuscripcionesActivas,
+  calcularIngresosEsperados,
 } from "../controllers/suscripcionController.js";
 import autenticarUsuario from "../middleware/authMiddleware.js";
+import autenticarUsuario from "../middleware/authMiddleware.js";
+import verificarAdmin from "../middleware/rolAdmin.js";
 
 const router = express.Router();
 
